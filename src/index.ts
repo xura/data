08a23d36c8722133f9bd811ba92aeb2104c2328e
Joe Badaczewski <joe307bad@gmail.com>
2019-10-06 23:27:55 -0400

    achieving working example of data with emporium

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,36 @@
 import { Achievement } from "./entities/Achievement";
-import { createConnection, Connection } from "typeorm";
+import { createConnection, Connection, getConnectionManager, getConnection } from "typeorm";
 import { initEmporium, Emporium } from '@xura/emporium';
 
-const connect = (): Promise<Connection> => createConnection({
-    type: "sqljs",
-    location: "emporium",
-    autoSave: true,
-    entities: [
-        Achievement
-    ],
-    logging: ['query', 'schema'],
-    synchronize: true
-});
+const connect = (connectionName: string): Promise<Connection> => {
+    return createConnection({
+        type: "sqljs",
+        location: "emporium",
+        autoSave: true,
+        entities: [
+            Achievement
+        ],
+        logging: ['query', 'schema'],
+        synchronize: true
+    })
+};
 
 initEmporium();
 
-const data = (connection: Connection) => ({
+const connection = () => getConnectionManager().get("default");
+
+const data = {
     achievements: new Emporium<Achievement>(
         connection,
         Achievement
     )
-});
+};
+
 
 export {
     connect,
-    data
+    data,
+    connection
 };
 
 
