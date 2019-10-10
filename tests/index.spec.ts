43e23e46d842e489dd5dbb0bde2ad124d1b70771
Joe Badaczewski <joe307bad@gmail.com>
2019-10-10 16:45:05 -0400

    achieving ~95% coverage

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
 import * as typeorm from 'typeorm'
 import {createSandbox, SinonSandbox, assert, createStubInstance, SinonStub} from 'sinon'
 import {expect} from 'chai';
-import {connect} from "../src";
+import {connect, connection, data} from "../src";
 
 class TypeormMock {
     public readonly sandbox: SinonSandbox;
@@ -20,7 +20,7 @@ class TypeormMock {
 describe('Xura | Data', () => {
     let typeormMock: TypeormMock;
 
-    it('connect function is called and returns connection data', async () => {
+    it('should return connection data when calling the connect function', async () => {
 
         const fakeConnectionData = { connectionData: 1 };
         typeormMock = new TypeormMock('createConnection', fakeConnectionData);
@@ -28,6 +28,29 @@ describe('Xura | Data', () => {
         expect(await connect()).to.equal(fakeConnectionData);
         assert.calledOnce(typeormMock.stub);
     });
+
+    it('should return the default connection when calling the connection function', async () => {
+
+        // Arrange
+        const fakeConnectionData = { connectionData: 1 };
+        const getStub = createStubInstance(typeorm.ConnectionManager);
+        getStub.get.resolves(fakeConnectionData);
+        typeormMock = new TypeormMock('getConnectionManager', getStub);
+
+        // Act
+        const connectionData = await connection();
+
+        // Assert
+        expect(connectionData).to.equal(fakeConnectionData);
+        // expect(await connection()).to.equal(fakeConnectionData);
+        // assert.calledOnce(typeormMock.stub);
+        // assert.calledOnce(typeormMock.stub.get);
+
+    });
+
+    it('should return an object when accessing the data object', function() {
+        expect(data).to.be.instanceOf(Object);
+    })
 
     afterEach(() => typeormMock.close())
 });
