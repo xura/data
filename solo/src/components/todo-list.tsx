aad9196d882783364a47c5c85638f6c9bf71baf9
Joe Badaczewski <joe307bad@gmail.com>
2019-12-30 16:30:16 -0500

    adding xura-list to solo data project

--- solo/src/components/todo-list.tsx
+++ solo/src/components/todo-list.tsx
@@ -26,8 +26,10 @@ export default class TodoList extends Component<{}, TodoListState> {
   }
 
   render({ }, { todos, text }) {
+    const items = ['Achievements', 'Users'];
     return (
       <form onSubmit={this.addTodo} action="javascript:">
+        <xura-list style="display: block; width: 33.3%;" items={items}></xura-list>
         <xura-text-input required="true" label="Achievement Name"></xura-text-input>
         <input value={text} onInput={this.setText} data-cy="new-todo-input" />
         <button type="submit" data-cy="todo-submit">Add</button>
