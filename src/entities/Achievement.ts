40135b1b30352e4b6789f77afd767decc8dab25b
Joe Badaczewski <joe307bad@gmail.com>
2020-01-02 22:20:03 -0500

    experimenting with adding FormComponent decorators to entity

--- src/entities/Achievement.ts
+++ src/entities/Achievement.ts
@@ -1,4 +1,11 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { reduce } from 'lodash';
+
+function FormComponent(serializer: { type: any }): (target: any, propertyKey: string) => void {
+    return function (target: any, propertyKey: string) {
+        Reflect.defineMetadata("metadata", serializer, target, propertyKey);
+    }
+}
 
 @Entity()
 export class Achievement {
@@ -7,5 +14,13 @@ export class Achievement {
     id?: number;
 
     @Column()
-    name: string = '';
+    @FormComponent({ type: '' })
+    static title: string = '';
 }
+
+const formComponents = reduce(Achievement, (acc, value, key) => ({
+    ...acc,
+    [key]: Reflect.getMetadata("metadata", Achievement, key)
+}), {})
+
+debugger;
