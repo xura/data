349e1ba4e674a62274376be81afef529fccd643a
Joe Badaczewski <joe307bad@gmail.com>
2020-01-28 21:21:28 -0500

    implementing refract and proper usage of functional components and props

--- solo/src/components/achievements.tsx
+++ solo/src/components/achievements.tsx
@@ -1,23 +1,20 @@
 import { Component, h } from 'preact';
 import { data } from '@xura/data';
+import { withEffects, toProps } from 'refract-preact-rxjs'
+import { map, flatMap } from 'rxjs/operators'
+import { pipe } from 'rxjs';
 
 import '@xura/components';
 
+const aperture = component => component.mount.pipe(
+  flatMap(_ => data.achievements.form('achievements-form')),
+  pipe(map(achievement => toProps({ achievement })))
+);
 
-export default class Acheivements extends Component {
+const Achievement = ({ achievement, pushEvent }) => (
+  <div>
+    <div id="achievements-form"></div>
+  </div>
+)
 
-  componentDidMount() {
-    data.achievements.form('achievements-form').subscribe(change => {
-      debugger;
-    })
-  }
-
-  render({ }, { }) {
-
-    return (
-      <div>
-        <div id="achievements-form"></div>
-      </div>
-    );
-  }
-}
+export default withEffects(aperture)(Achievement)
