e5e8d3f8e76a6fff29f3eaa07f096184e27aa98f
Joe Badaczewski <joe307bad@gmail.com>
2020-02-01 22:17:44 -0500

    wiring up navigation events

--- solo/src/components/root.tsx
+++ solo/src/components/root.tsx
@@ -1,19 +1,88 @@
-import { h } from 'preact';
+import { h, createRef } from 'preact';
+import { useState } from 'preact/hooks';
 import { withEffects, toProps } from 'refract-preact-rxjs'
-import { map, flatMap } from 'rxjs/operators'
-import { pipe } from 'rxjs';
-import { style } from "typestyle";
+import { map, startWith, flatMap, tap } from 'rxjs/operators'
+import { of, fromEvent, merge } from 'rxjs';
+import { capitalCase } from "change-case";
+import { data } from '@xura/data';
 
 import '@xura/components';
 import AchievementForm from './achievement';
 
+const items = Object.keys(data).map(_ => capitalCase(_))
+const drawer = createRef();
 
-const Root = () => (
-    <xura-drawer title="Xura | Data">
-        <span slot='content'>
-            <AchievementForm />
-        </span>
-    </xura-drawer>
-)
+const aperture = (component, props) => {
+    const activeTab$ = component.observe('setActiveTab')
+    debugger;
+    return merge(
+        component.mount.pipe(
+            flatMap((_: any) => fromEvent(drawer.current, 'navigate').pipe(
+                map((evt: any) => ({
+                    type: 'NAVIGATION',
+                    replace: true,
+                    state: evt.detail
+                }))))),
+        of({
+            type: 'NAVIGATION',
+            replace: true,
+            state: props.activeTab
+        }),
 
-export default Root;
+        activeTab$.pipe(
+            map(activeTab => ({
+                type: 'NAVIGATION',
+                replace: false,
+                state: activeTab
+            }))
+        ),
+
+        fromEvent(window, 'popstate').pipe(
+            map((evt: any) => ({
+                type: 'STATE',
+                state: evt.state || { activeTab: 'ObiWan' }
+            }))
+        )
+    )
+}
+
+const handler = ({ setActiveTab }) => effect => {
+    switch (effect.type) {
+        case 'NAVIGATION':
+            const path = document.location.pathname
+            const search = effect.state
+                ? `?entity=${effect.state}`
+                : ''
+            const methodName = effect.replace ? 'replaceState' : 'pushState'
+            window.history[methodName](effect.state, null, `${path}${search}`)
+            return
+
+        case 'STATE':
+            return setActiveTab(effect.state)
+
+        default:
+            return
+    }
+}
+
+const Root = ({ activeTab, setActiveTab }) => {
+    return (
+        <xura-drawer ref={drawer} activeItem={activeTab} items={items} title="Xura | Data">
+            <span slot='content'>
+                <AchievementForm />
+            </span>
+        </xura-drawer>
+    )
+}
+
+const RootWithEffects = withEffects(aperture, { handler })(Root);
+
+export default ({ entity }) => {
+    const [activeTab, setActiveTab] = useState(entity || items[0])
+
+    return (
+        <RootWithEffects activeTab={activeTab} setActiveTab={setActiveTab} />
+    );
+}
+
+
--- solo/src/index.tsx
+++ solo/src/index.tsx
@@ -3,12 +3,15 @@ import 'reflect-metadata'
 import { render, h } from 'preact';
 import Root from './components/root';
 import { connect, data } from '@xura/data';
+import * as parser from 'url-parameter-parser';
 
 connect().then(_ => {
   // @ts-ignore
   window.d = data;
+  const { entity } = parser(document.location.search);
+
   render(
-    <Root />,
+    <Root entity={entity} />,
     document.querySelector('#root')
   );
 });
